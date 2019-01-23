3ba15516fe6f94c7fe5c768cf76eef01937b5809
Seth Wax <sethlwax@gmail.com>
2019-01-23 14:51:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,6 +65,10 @@ class Student
     rows.map {|row| new_from_db(row)}
   end
 
+  def self.first_student_in_grade_10
+    first_X_students_in_grade_10(1)
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
