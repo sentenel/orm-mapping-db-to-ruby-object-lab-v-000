f71bbe271d5a236193ccdc053ab386666fd3a469
Seth Wax <sethlwax@gmail.com>
2019-01-23 14:50:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,6 +52,19 @@ class Student
     rows.map {|row| new_from_db(row)}
   end
 
+  def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = 10
+      ORDER BY id ASC
+      LIMIT ?
+    SQL
+
+    rows = db[:conn].execute(sql, x)
+
+    rows.map {|row| new_from_db(row)}
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
