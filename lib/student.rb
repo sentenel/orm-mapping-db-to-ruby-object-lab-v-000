4b73336d16a2e4e90f146c6cd993d6862df36e22
Seth Wax <sethlwax@gmail.com>
2019-01-23 14:42:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,8 +10,13 @@ class Student
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students
+    SQL
+
+    rows = DB[:conn].execute(sql)
+    
+    rows.map {|row| Student.new_froM_db(row)}
   end
 
   def self.find_by_name(name)
